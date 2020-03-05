015589b4bc67e4936e4d4ebd0273603e8ee15cdc
Bobby Uno <robert.y.uno@gmail.com>
2020-03-05 05:24:35 +0000

    injection exercise complete

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -25,4 +25,21 @@ ORDER BY teacher;
 });
 
 
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+  `;
+
+  const values = [`%${process.argv[2]}%`];
+
+  console.log(values)
+  pool.query(queryString, values)
+  .then( res => {
+    console.log(res.rows);
+  });
 
